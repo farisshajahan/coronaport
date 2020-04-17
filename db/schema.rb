cfb118684b89c43765e44717673afd43a0dc2a29
aswinmohanme <dev.aswinmohan@gmail.com>
2020-04-17 17:09:24 +0530

    Create new Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_082213) do
+ActiveRecord::Schema.define(version: 2020_04_17_113503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,41 @@ ActiveRecord::Schema.define(version: 2020_04_08_082213) do
     t.date "date_of_contact"
     t.string "tracking_type"
     t.text "panchayat_feedback"
+    t.boolean "covid_19_was_confirmed"
+    t.boolean "contact_with_suspected_14_days"
+    t.boolean "contact_with_confirmed_14_days"
+    t.boolean "travel_non_risk_area_4_6"
+    t.boolean "travel_non_risk_area_0_3"
+    t.boolean "travel_risk_area_4_6"
+    t.boolean "travel_risk_area_0_3"
+    t.boolean "cormobidity_hypertension"
+    t.boolean "cormobidity_diabetes"
+    t.boolean "cormobidity_cardio"
+    t.boolean "cormobidity_liver"
+    t.boolean "cormobidity_renal"
+    t.boolean "cormobidity_hypercholestrolemia"
+    t.boolean "cormobidity_hiv"
+    t.boolean "cormobidity_cancer"
+    t.boolean "cormobidity_pregnancy"
+    t.boolean "cormobidity_respiratory"
+    t.boolean "regular_treatment"
+    t.boolean "surgeries_three_years"
+    t.boolean "on_immunosuppresants"
+    t.boolean "history_of_transplants"
+    t.boolean "asomia_cold_rhinorrhea"
+    t.boolean "sorethroat_diarrhoea"
+    t.boolean "fever_cough"
+    t.boolean "breathing_difficulty"
+    t.bigint "user_id"
     t.index ["panchayat_id"], name: "index_contacts_on_panchayat_id"
     t.index ["phone"], name: "index_contacts_on_phone", unique: true
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
+  create_table "districts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "medical_reqs", force: :cascade do |t|
@@ -86,6 +119,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_082213) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
     t.bigint "panchayat_id"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["panchayat_id"], name: "index_users_on_panchayat_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -93,6 +127,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_082213) do
 
   add_foreign_key "calls", "contacts"
   add_foreign_key "calls", "users"
+  add_foreign_key "contacts", "users"
   add_foreign_key "medical_reqs", "contacts"
   add_foreign_key "non_medical_reqs", "contacts"
 end
